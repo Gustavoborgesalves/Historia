bcf3fffd614e31d129992139a388f8c58c2c7090
Gustavoborgesalves <160506060+Gustavoborgesalves@users.noreply.github.com>
2024-08-05 11:50:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const caixaPrincipal =  document.querySelector(".caixa-principal");
+const caixaPerguntas =  document.querySelector(".caixa-perguntas");
+const caixaAlternativas =  document.querySelector(".caixa-alternativa");
+const caixaResultado =  document.querySelector(".caixa-resultado");
+const textoResulado =  document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "escrever"
+        alternativas: [
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            },
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            }
+        ]
+    },
+    {
+        enunciado: "escrever"
+        alternativas: [
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            },
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            }
+        ]
+    },
+    {
+        enunciado: "escrever"
+        alternativas: [
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            },
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            }
+        ]
+    },
+    {
+        enunciado: "escrever"
+        alternativas: [
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            },
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            }
+        ]
+    },
+    {
+        enunciado: "escrever"
+        alternativas: [
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            },
+            {
+                texto: "escrever",
+                afirmacao: "escrever",
+            }
+        ]
+    },
+]
